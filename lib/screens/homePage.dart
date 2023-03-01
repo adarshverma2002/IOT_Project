afb3728f66a77345e90f234b8bc4c3d4c7516445
Adarsh Verma <vadarsh931@gmail.com>
2023-03-01 19:30:52 +0530

    HomePage changes

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -11,7 +11,7 @@ class powerUnitsScreen extends StatefulWidget {
 class _powerUnitsScreenState extends State<powerUnitsScreen> {
   @override
   Widget build(BuildContext context) {
-    double _value = 112.0;
+    double _value = 5000.0;
     return Scaffold(
       // appBar: AppBar(
       //   backgroundColor: Color(0xff0bd0a3),
@@ -19,57 +19,95 @@ class _powerUnitsScreenState extends State<powerUnitsScreen> {
       body: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
+          const SizedBox(
             height: 30,
           ),
           Container(
             height: 300,
             width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 boxShadow: [BoxShadow(blurRadius: 25)],
                 color: Color(0xff0bd0a3),
                 borderRadius:
                     BorderRadius.vertical(bottom: Radius.circular(15))),
             child: Column(
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                Text(
-                  "Total energy consumed",
+                const Text(
+                  "",
                   style: TextStyle(
                       color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Tilt'),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 50,
                 ),
                 AnimatedFlipCounter(
                   value: _value,
-                  duration: Duration(milliseconds: 900),
+                  duration: const Duration(milliseconds: 1000),
                   fractionDigits: 2,
-                  textStyle: TextStyle(
+                  textStyle: const TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: 60),
                 ),
-                Text(
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
                   "Units",
                   style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.w900,
-                      fontSize: 19),
+                      fontSize: 25),
                 ),
               ],
             ),
           ),
-          Container(
-              child: Column(
+          Column(
             children: [
-              Text("Here comes car information"),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: const Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Amount to be paid ",
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 30,
+                          fontFamily: 'Tilt'),
+                    )),
+              ),
+              const SizedBox(height: 60),
+              const AnimatedFlipCounter(
+                prefix: '₹',
+                value: 20,
+                duration: Duration(milliseconds: 1000),
+                textStyle: TextStyle(
+                    color: Color(0xff0bd0a3),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              // SizedBox(height: 290),
+              ElevatedButton(
+                  onPressed: () {
+                    print("Payment button pressed");
+                  },
+                  child: const Text(
+                    "   Proceed to Pay   ",
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff0bd0a3)))
             ],
-          )), // This container is for Car details or some other info like amount to be payed
+          ), // This container is for Car details or some other info like amount to be payed
         ],
       ),
     );
